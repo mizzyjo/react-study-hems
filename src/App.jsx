b2920071852cca8906f02fe949516e274cf58b23
minj <minj@ksmartech.com>
2024-12-07 21:22:26 +0900

    fix: modify redirect to 'dashboard' page only when the path is root

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useEffect } from 'react'
 import { LNB } from './components/common/LNB'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { useNavigate } from 'react-router'
 function App() {
     let navigate = useNavigate()
+    const { pathname } = useLocation()
 
     useEffect(() => {
-        navigate('/dashboard')
+        if (pathname === '/') {
+            navigate('/dashboard')
+        }
     }, [])
-    
+
     return (
         <>
             <LNB />
