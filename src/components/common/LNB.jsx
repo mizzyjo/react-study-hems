b815edeec84e6259f576cb47d2a5bf083cd4372d
minj <minj@ksmartech.com>
2024-12-07 19:11:52 +0900

    feat: add LNB component

---
+++ src/components/common/LNB.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const LNB = props => (
+    <header>
+        <ul>
+            <li>
+                현황
+                <ul>
+                    <li>사이트 현황</li>
+                </ul>
+            </li>
+            <li>
+                관리
+                <ul>
+                    <li>사이트 관리</li>
+                </ul>
+            </li>
+        </ul>
+    </header>
+)
