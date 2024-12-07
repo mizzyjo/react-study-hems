4c8375a6510c48511164e515bd03dbf685400295
minj <minj@ksmartech.com>
2024-12-07 23:00:03 +0900

    chroe: 테스트 페이지에 TanStack Query 사용기능 추가

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,15 +1,33 @@
 import React, { useEffect } from 'react'
 import TestClient from '../components/demo/TestClient'
+import TestChart2 from '../components/demo/TestChart2'
 import { useHemsApi } from '../context/HemsApiContext'
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
 
 export default function TestPage() {
     const { hems } = useHemsApi()
+    const keyword = 'building'
     useEffect(() => {
         console.log('test page: ', hems.buildingStats())
     }, [])
+
+    const {
+        isLoading,
+        error,
+        data,
+    } = useQuery({
+        queryKey: ['buildingStats'],
+        queryFn: () => hems.buildingStats(keyword),
+        staleTime: 1000 * 60 * 1,
+    })
+
+    console.log('tanstack: ', isLoading, data)
+
     return (
         <div>
             <h1>Test Page</h1>
+            <TestChart2/>
             <TestClient />
         </div>
     )
