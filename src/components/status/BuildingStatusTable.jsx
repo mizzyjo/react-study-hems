d0dd52741d5846544a8341705ee6a4e33c4352dc
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-13 10:14:08 +0900

    feat: BuildingStatusTable 컴포넌트에 tableTite 변수 추가

--- src/components/status/BuildingStatusTable.jsx
+++ src/components/status/BuildingStatusTable.jsx
@@ -2,32 +2,33 @@ import React from 'react'
 
 export const BuildintStatusTable = ({ data }) => {
     return (
-        <>
-            <table>
-                <thead>
-                    <tr>
-                        {columnHeaders.map((item, idx) => {
-                            return <th key={idx}>{item}</th>
-                        })}
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, idx) => {
-                        return (
-                            <tr key={idx}>
-                                <td>{item.buildingName}</td>
-                                <td>{item.siteName}</td>
-                                <td>{item.recvPower}</td>
-                                <td>{item.nowRecvPower}</td>
-                                <td>{item.prevRecvPower}</td>
-                            </tr>
-                        )
+        <table>
+            <caption>{tableTitle}</caption>
+            <thead>
+                <tr>
+                    {columnHeaders.map((item, idx) => {
+                        return <th key={idx}>{item}</th>
                     })}
-                </tbody>
-            </table>
-        </>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item, idx) => {
+                    return (
+                        <tr key={idx}>
+                            <td>{item.buildingName}</td>
+                            <td>{item.siteName}</td>
+                            <td>{item.recvPower}</td>
+                            <td>{item.nowRecvPower}</td>
+                            <td>{item.prevRecvPower}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
     )
 }
+
+const tableTitle = '유닛리스트'
 
 const columnHeaders = [
     '건물명',
