351b755676d9fe288f2c3f0d37738981e44aa86c
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-13 16:23:14 +0900

    feat: add MixedChart component

---
+++ src/components/common/MixedChart.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Line } from 'react-chartjs-2'
+import _ from 'lodash'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+
+export const MixedChart = ({ chartTitle, data, options }) => {
+    return (
+        <>
+            <h2>{chartTitle}</h2>
+            <Line data={data} options={options} />
+        </>
+    )
+}
+
