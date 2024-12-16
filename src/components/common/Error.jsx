2a2ae26065197c0a219e4348ca299e05bd53d0b4
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-16 13:12:52 +0900

    feat: add Error and Loading components

---
+++ src/components/common/Error.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Error = ({ error }) => (
+    <>
+        <p>에러가 발생했습니다.</p>
+        {console.log('error:: ', error)}
+    </>
+)
---
+++ src/components/common/Loading.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const Loading = props => <p>데이터를 불러오는 중입니다...</p>
