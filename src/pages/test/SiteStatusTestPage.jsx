fb24028563a2c7b300d1b585a062041204527ca0
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-16 16:58:20 +0900

    feat: 사이트 현황 테스트 페이지_Boxplot chart 추가

--- src/pages/test/SiteStatusTestPage.jsx
+++ src/pages/test/SiteStatusTestPage.jsx
@@ -5,6 +5,8 @@ import { produce } from 'immer'
 import { CustomChart } from '../../components/common/CustomChart'
 import { FaBeer } from "react-icons/fa";
 import PowerCard from '../../components/common/PowerCard'
+import { BoxplotChart } from '../../components/common/BoxplotChart'
+import { Loading } from '../../components/common/Loading'
 
 export default function SiteStatusTestPage() {
     const { hems } = useHemsApi()
@@ -66,6 +68,48 @@ export default function SiteStatusTestPage() {
         }
     }, [isEssDataLoading, isPvDataLoading, isBuildDataLoading])
 
+    // 시간대별 EV 충전기 연결 현황 그래프(boxplot chart 그리기)
+    const {
+        isLoading: isEvConnectionDataLoading,
+        error: evConnectionDataError,
+        data: evConnectionResultData,
+    } = useQuery({
+        queryKey: ['evConnectionStatus'],
+        queryFn: () => hems.evConnectionStatsData(),
+        staleTime: 1000 * 60 * 1,
+    })
+
+    const [boxplotConfig, setBoxplotConfig] = useState(initialBoxPlotConfig)
+
+
+    useEffect(() => {
+        if(evConnectionResultData){
+            const evConnStatus = evConnectionResultData.resultData.evTimeList.map(item => {
+                return {
+                    min: item.minTime || 0,
+                    median: item.avgTime || 0,
+                    max: item.maxTime || 0,
+                    q1: item.minTime + (item.avgTime - item.minTime) / 2 || 0,
+                    q3: item.avgTime + (item.maxTime - item.avgTime) / 2 || 0,
+                }
+            })
+
+            const evCnt =  evConnectionResultData.resultData.evTimeList.map(item => item.cnt)
+            
+            setBoxplotConfig(
+                produce(boxplotConfig, darft => {
+                    darft.data.datasets[0].data = evConnStatus
+                    darft.data.datasets[1].data = evCnt
+                })
+            )
+
+        }
+    }, [evConnectionResultData])
+
+    if(isEvConnectionDataLoading) {
+        return <Loading />
+    }
+
     return (
         <div>
             <h1>SiteStatus Test Page</h1>
@@ -82,6 +126,13 @@ export default function SiteStatusTestPage() {
                             options={chartConfig.options}
                         />
                 )
+            )}
+            {evConnectionResultData && (
+                <BoxplotChart
+                    chartTitle={'시간대별 EV 충전기 연결 현황'}
+                    data={boxplotConfig.data}
+                    options={boxplotConfig.options}
+                />
             )}
         </div>
     )
@@ -147,6 +198,70 @@ const initialChartConfig = {
     },
 }
 
+const initialBoxPlotConfig = {
+    type: 'boxplot',
+    data: {
+        labels: Array.from({ length: 24 }, (_, i) => String(i)),
+        datasets: [
+            {
+                label: '시간',
+                data: [],
+                backgroundColor: '#239EC4',
+                borderColor: '#239EC4',
+                borderWidth:3,
+                order: 2,
+                yAxisID: 'y-left', // 첫 번째 데이터셋은 왼쪽 y축을 사용
+            },
+            {
+                type: 'line',
+                label: 'EV 차량',
+                data: Array(24).fill(0),
+                backgroundColor: '#A0D6B4',
+                borderColor: '#A0D6B4',
+                borderWidth: 3,
+                order: 1,
+                yAxisID: 'y-right', // 두 번째 데이터셋은 오른쪽 y축을 사용
+            },
+        ],
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Box',
+            },
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        const data = context.raw
+                        if ( context.dataset.label === '시간') {
+                            return ` 최소: ${data.min}분 / 평균: ${data.median}분 / 최대: ${data.max}분`
+                        } else {
+                            return ` EV 차량: ${data}`
+                        }
+                    },
+                },
+                
+            },
+        },
+        responsive: true,
+        scales: {
+             // x축 타이틀 설정
+             x: {
+                title: {
+                    display: true,
+                    text: '(시간)',
+                    // align: 'start', // 타이틀 왼쪽 정렬
+                },
+            },
+            // y축을 오른쪽에 세팅
+            'y-right': {
+                // stacked: true,
+                position: 'right',
+            },
+        },
+    },
+}
 
 
 /**
