e4071698a9c66968ccda77fa428e18220716912e
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-16 17:29:19 +0900

    chore: Error 컴포넌트 문구 변경

--- src/components/common/Error.jsx
+++ src/components/common/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Error = ({ error }) => (
     <>
-        <p>에러가 발생했습니다.</p>
+        <p>데이터를 불러오는 중 오류가 발생했습니다. 다시 시도해주세요..</p>
         {console.log('error:: ', error)}
     </>
 )
