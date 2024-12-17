85dcf723ef792dafb3027b3ec73715146f76a172
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-17 16:26:10 +0900

    feat: 건물 관리 페이지 추가

--- src/pages/management/BuildingMgmt.jsx
+++ src/pages/management/BuildingMgmt.jsx
@@ -1,10 +1,59 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { Loading } from '../../components/common/Loading'
+import { QUERY_KEYS } from '../../config/querykeys'
+import { useHemsApi } from '../../context/HemsApiContext'
+import { Error } from '../../components/common/Error'
+import { BuildintStatusTable } from '../../components/status/BuildingStatusTable'
 
 export default function BuildingMgmt() {
+    const { hems } = useHemsApi()
+
+    // 빌딩 리스트
+    const {
+        isLoading: isBuildingListLoading,
+        error: buildingListError,
+        data: buildingListData,
+    } = useQuery({
+        queryKey: [QUERY_KEYS.buildingStatusList],
+        queryFn: () => hems.buildingStatusList(),
+        staleTime: 1000 * 60 * 1,
+    })
+
+    const buildingList = buildingListData?.list || initialBuildingList
+
+    if (isBuildingListLoading) {
+        return <Loading />
+    }
+    
+    if (buildingListError) {
+        return <Error error={{ buildingListError }} />
+    }
+    
+    
     return (
         <div>
-            <h1>Building Management Page</h1>
+            <h1>건물 관리</h1>
+            <BuildintStatusTable data={buildingList}/>
         </div>
     )
 }
 
+const initialBuildingList = [
+    {
+        buildingId: 'S0001BLD01',
+        buildingName: '테스트건물',
+        buildingSeq: '1',
+        nowRecvPower: 0,
+        page: 1,
+        pageSize: 5,
+        pagination: null,
+        prevRecvPower: 12,
+        recordSize: 10,
+        recvPower: 0,
+        siteId: 'S0001',
+        siteName: '테스트사이트',
+        siteSeq: '1',
+        siteType: null,
+    },
+]
