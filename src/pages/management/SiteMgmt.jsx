c11b0c505893d16f709d7ba883e675b49e842def
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-17 17:03:10 +0900

    feat: 사이트 관리 페이지 추가

--- src/pages/management/SiteMgmt.jsx
+++ src/pages/management/SiteMgmt.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { useHemsApi } from '../../context/HemsApiContext'
+import { QUERY_KEYS, STALE_TIME } from '../../config/queryConfig'
+import { useQuery } from '@tanstack/react-query'
+import { SiteMgmtTable } from '../../components/management/SiteMgmtTable'
+import { Loading } from '../../components/common/Loading'
+import { Error } from '../../components/common/Error'
+
 
 export default function SiteMgmt() {
+    const { hems } = useHemsApi()
+    
+    const {
+        isLoading: isSiteListLoading,
+        error: siteListError,
+        data: siteListData,
+    } = useQuery({
+        queryKey: [QUERY_KEYS.siteList],
+        queryFn: () => hems.siteList(),
+        staleTime: STALE_TIME.ONE_MINUTE,
+    })
+
+    const siteList = siteListData?.list || []
+
+    if (isSiteListLoading) {
+        return <Loading />
+    }
+    
+    if (siteListError) {
+        return <Error error={siteListError} />
+    }
+
     return (
         <div>
-            <h1>Site Management Page</h1>
+            <h1>사이트 관리</h1>
+            <SiteMgmtTable data={siteList} />
         </div>
     )
 }
-
