68d31137f062d4302327f5f304a558a00cae8713
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-17 14:05:44 +0900

    feat: Tanstack Query 키 config 추가

--- src/config/querykeys.js
+++ src/config/querykeys.js
@@ -1,10 +1,23 @@
 // Tanstack Query 키
 
 export const QUERY_KEYS = {
+    // 사이트 현황
+    essData: 'essData',
+    pvData: 'pvData',
+    buildData: 'buildData',
+    evConnectionStatus: 'evConnectionStatus',
+
+    // 빌딩 현황
+    buildingTotalInfo: 'buildingTotalInfo',
+    buildingStats: 'buildingStats',
+    consumeRecvPower: 'consumeRecvPower',
+    buildingStatusList: 'buildingStatusList',
+
+    // 전기 요금제 관리
     epriceByDay: 'epriceByDay',
     epriceByYear: 'epriceByYear',
 }
 
 /** 사용 예
- * queryKey: [QUERY_KEYS.priceByDay]
- */
+    queryKey: [QUERY_KEYS.priceByDay]
+*/
