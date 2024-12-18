60b2548f863f192074d0bff2cde0d6c3ec63cb90
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-18 14:43:28 +0900

    feat: PowerCard 컴포넌트에 PostCSS 적용

--- src/components/common/PowerCard.jsx
+++ src/components/common/PowerCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import styles from './PowerCard.module.css'
 
 export default function PowerCard({ title, kwValue, description, img: ImgComponent }) {
     return (
-        <div>
-            <h3>{title}</h3>
-            <p>{kwValue}</p>
-            {description &&  <p>{description}</p>}
-            {ImgComponent && <ImgComponent size={30}/>}
+        <div className={styles.card}>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.kwValue}>{kwValue}</p>
+            {description && <p className={styles.description}>{description}</p>}
+            {ImgComponent && (
+                <span className={styles.imgWrapper}>
+                    <ImgComponent size={30} />
+                </span>
+            )}
         </div>
     )
 }
