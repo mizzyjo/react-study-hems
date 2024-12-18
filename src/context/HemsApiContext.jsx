73f8e72fddc66c6228195efbf77c876df372f025
DESKTOP-LHJQ2GH\ksmartech <minj@ksmartech.com>
2024-12-18 14:50:05 +0900

    feat: context에 실제 client 인스턴스 추가

--- src/context/HemsApiContext.jsx
+++ src/context/HemsApiContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useContext } from 'react'
 import FakeHemsClient from '../api/fakeHemsClient'
+import hemsClient from '../api/hemsClient'
 import Hems from '../api/hems'
 
 const HemsApiContext = createContext()
 
-const client = new FakeHemsClient()
+const client = new hemsClient()
+// const client = new FakeHemsClient()
+
 const hems = new Hems(client)
 
 export function HemsApiProvider({ children }) {
